f68c5a9c5521cf84ba44989af5fd890fc8341a67
Scott Nonnenberg <scott@signal.org>
2023-08-07 16:02:48 -0700

    Remove MyObject and unnecessary comments

--- NativeExtension.cc
+++ NativeExtension.cc
@@ -2,15 +2,11 @@
 
 using v8::FunctionTemplate;
 
-// NativeExtension.cc represents the top level of the module.
-// C++ constructs that are exposed to javascript are exported here
+// The top level of the module
 
 NAN_MODULE_INIT(InitAll) {
   Nan::Set(target, Nan::New("sendDummyKeystroke").ToLocalChecked(),
     Nan::GetFunction(Nan::New<FunctionTemplate>(sendDummyKeystroke)).ToLocalChecked());
-
-  // Passing target down to the next NAN_MODULE_INIT
-  MyObject::Init(target);
 }
 
 NODE_MODULE(NativeExtension, InitAll)
